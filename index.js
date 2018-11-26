7eaca736a686e475818427b8c36e6c4f696b567e
Jesse Monroy <jessemonroy650@yahoo.com>
2018-11-26 12:33:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var geo = {
+	getGeo: function() {		
+		navigator.geolocation.getCurrentPosition(
+			  geo.onSuccess,
+			  geo.onError,
+			  {maximumAge:0, timeout: 5000, enableHighAccuracy: false});		
+		
+	},
+	onSuccess: function(position) {
+		alert('GOT location');
+		
+		alert(position.coords.latitude + ' --- ' + position.coords.longitude);
+	},
+	onError: function(error) {
+		alert('error getting geo!');
+	}	
+};
+ 
+ var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+		geo.getGeo();
+    }
+};
+
+
+
+app.initialize();
